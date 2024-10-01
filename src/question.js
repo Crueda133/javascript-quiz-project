8ec281cc4cf0ba4f82fac796544492fe1b8bcb09
Cristina Rueda <crueda133@hotmail.com>
2024-10-01 20:13:34 +0200

    done done doneeee

--- src/question.js
+++ src/question.js
@@ -1,19 +1,19 @@
 class Question {
-    // YOUR CODE HERE:
-    
-    constructor (text, choices, answer, difficulty) {
-        this.text = text;
-        this.choices = choices;
-        this.answer = answer;
-        this.difficulty = difficulty;
-    }
+  // YOUR CODE HERE:
 
-    shuffleChoices() {
-  for (let i = 0; i < this.choices.length; i++) {
-const curentChoice = this.choices[i];
-const randomIndex = Math.floor(Math.random() *this.choices.length);
-this.choices.splice(i, 1);
-this.choices.splice(randomIndex, 0, curentChoice);
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
   }
+
+  shuffleChoices() {
+    for (let i = 0; i < this.choices.length; i++) {
+      const curentChoice = this.choices[i];
+      const randomIndex = Math.floor(Math.random() * this.choices.length);
+      this.choices.splice(i, 1);
+      this.choices.splice(randomIndex, 0, curentChoice);
     }
+  }
 }
